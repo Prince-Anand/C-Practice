519d1caad683bdd1dc2cf66c8e416c162eae1b9f
Prince Anand <pk933466@gmail.com>
2024-03-12 23:40:33 +0530

    Day 1 - First Program of Hello world with comments on each line

---
+++ Day 1/First_Program.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // Include the standard input-output header file, which contains functions like printf() and scanf().
+// its like importing math.py for maths function in python
+
+int main() // The main function where the execution of the program begins
+{
+    printf("Hello World"); // printf() is a function which is already coded in c library stdio.h
+    return 0;              // Return 0 to indicate successful execution of the program without any error.
+}
+
+// for printing output in terminal
+// move to folder of the code file in the directory with cd commands
+// write 1st -> gcc file.c
+// then, write 2nd line -> .\a.exe
