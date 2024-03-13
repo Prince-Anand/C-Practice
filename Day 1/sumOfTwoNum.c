f6ae7f2cd4bb7a118f58f7fc03c7570e90826ebf
Prince Anand <pk933466@gmail.com>
2024-03-13 10:59:41 +0530

    Sum of two given numbers

---
+++ Day 1/sumOfTwoNum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b; // declaring data type for variable a and b
+    a = 10;   // assigning values to variable a and b
+    b = 20;
+    printf("%d", a + b); // %d is a formatting character used to print output of diff. data types in c
+    return 0;
+}
+
+// variable is a container where constants are filled in this container
