78975ef5d50d635291e1958a83475952281fecf4
Prince Anand <pk933466@gmail.com>
2024-03-13 10:51:13 +0530

    Understanding Escape Sequence \n (non-printing characters)

---
+++ Day 1/escapeseq.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello-");
+    printf("World"); // Hello-World on same line as compiler does not print to new line by itself
+    printf("\n");    // Output Jumps on next line
+    printf("This is on new line.");
+    return 0;
+}
