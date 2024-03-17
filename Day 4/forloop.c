0a7fc8e072486cb6aa0bf926627ba0bbe472dcb6
Prince Anand <pk933466@gmail.com>
2024-03-17 22:45:19 +0530

    Day 4 - For Loop Syntax and print first 10 natural numbers

---
+++ Day 4/forloop.c
@@ -0,0 +1,12 @@
+/*Syntax
+ for (initialization;condition;updation){
+ }
+*/
+#include <stdio.h>
+//print first 10 numbers
+int main() {
+    for (int x =1; x<=10;x++){
+        printf("%d\n",x);
+    }
+    return 0;
+}
