b8bedf8332ca1017034e6b0c724f3b871a2be269
Prince Anand <pk933466@gmail.com>
2024-03-18 21:37:03 +0530

    Day 5 - Practice

---
+++ Day 5/printOddNumFrom1to100.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    for (int x= 1; x<=100; x= x+2){ //loop runs 100 times
+        printf("%d ",x);
+    }
+    return 0;
+}
+
+//even from 1 to 100
+
+// int main() {
+//     for (int x= 2; x<=100; x= x+2){ //loop runs 100 times
+//         printf("%d ",x);
+//     }
+//     return 0;
+// }
