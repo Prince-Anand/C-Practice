1de426ff199bf6c28e34c0ec9439ed6ed6a0c29a
Prince Anand <pk933466@gmail.com>
2024-03-18 23:07:20 +0530

    Day 5 - AP -> 4,7,10,13... upto n terms

---
+++ Day 5/ap_4_7_10_nterms.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+//Print the AP - 4,7,10,13.. upto ‘n’ terms
+int main() {
+    int x;
+    printf("No. of terms: ");
+    scanf("%d",&x);
+    for (int i = 4; i <= 3*x+1; i=i+3)// 3 is the cd of AP and loop until last term and loop runs x times
+    {
+        printf("%d",i);
+        if (i<3*x+1) printf(",");// doesnt print ',' when i==3*x+1
+    }
+    
+    return 0;
+}
