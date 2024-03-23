f20e1281dbc11e9f5eb0e7bf00562f41d385167d
Prince Anand <pk933466@gmail.com>
2024-03-23 23:39:19 +0530

    added comments

--- Day 5/evennumberusingContinue.c
+++ Day 5/evennumberusingContinue.c
@@ -2,7 +2,7 @@
 int main() {
     for (int i =1 ; i<=100;i++){
         if (i%2!=0){
-            continue;
+            continue;//skips the loop if the no. is not divisible by 2 / odd no.
         }
         printf("%d ",i);
     }
--- Day 5/oddnumberusingCONTINUE.c
+++ Day 5/oddnumberusingCONTINUE.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    for (int i =1 ; i<=100;i++){
+    for (int i =1 ; i<=100;i++){//loop starts with 1 and goes till 100 with increase in value of i by 1 each time
         if (i%2==0){
-            continue;
+            continue;//skips the loop if the no. is divisible by 2 / even no.
         }
         printf("%d ",i);
     }
