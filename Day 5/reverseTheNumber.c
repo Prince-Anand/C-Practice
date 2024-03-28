c1d8646553c084953924a008dc78ed2f789259f2
Prince Anand <pk933466@gmail.com>
2024-03-28 23:45:29 +0530

    Program to print reverse of a given number

--- Day 5/reverseTheNumber.c
+++ Day 5/reverseTheNumber.c
@@ -2,13 +2,14 @@
 
 int main() {
     int x;
-    printf("The Number: ");
+    printf("Enter Number: ");
     scanf("%d",&x);
     int count=0;
     while (x>0){
         count += x%10;
         x/=10;
+        count*=10;
     }
-    printf("Sum. of Digits : %d",count);
+    printf("Sum. of Digits : %d",count/10);
     return 0;
 }
